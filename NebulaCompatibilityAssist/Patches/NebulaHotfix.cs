fc2cba2c48b6e801e2b14dd70d91e1723de60051
starfish <ac355deny@gmail.com>
2022-10-29 12:48:17 +0800

    NebulaHotfix for 0.8.12

--- NebulaCompatibilityAssist/Patches/NebulaHotfix.cs
+++ NebulaCompatibilityAssist/Patches/NebulaHotfix.cs
@@ -4,16 +4,13 @@ using System.Reflection.Emit;
 using System.Collections.Generic;
 using NebulaAPI;
 using NebulaModel.Networking;
-using NebulaModel.Packets.Logistics;
 using System.Reflection;
-using NebulaModel.Packets.Planet;
-using System.Threading.Tasks;
 
 namespace NebulaCompatibilityAssist.Patches
 {
     public static class NebulaHotfix
     {
-        private const string NAME = "NebulaMultiplayerMod";
+        //private const string NAME = "NebulaMultiplayerMod";
         private const string GUID = "dsp.nebula-multiplayer";
         private static bool isPatched = false;
 
@@ -25,10 +22,10 @@ namespace NebulaCompatibilityAssist.Patches
             try
             {
                 System.Version nebulaVersion = pluginInfo.Metadata.Version;
-                if (nebulaVersion.Major == 0 && nebulaVersion.Minor == 8 && nebulaVersion.Build == 11)
+                if (nebulaVersion.Major == 0 && nebulaVersion.Minor == 8 && nebulaVersion.Build == 12)
                 {
-                    Patch0811(harmony);
-                    Log.Info("Nebula hotfix 0.8.11 - OK");
+                    Patch0812(harmony);
+                    Log.Info("Nebula hotfix 0.8.12 - OK");
                 }
             }
             catch (Exception e)
@@ -38,16 +35,18 @@ namespace NebulaCompatibilityAssist.Patches
             }
         }
 
-        private static void Patch0811(Harmony harmony)
+        private static void Patch0812(Harmony harmony)
         {
-            Type classType = AccessTools.TypeByName("NebulaWorld.Logistics.CourierManager");
-            harmony.Patch(AccessTools.Method(classType, "GameTick"), null, null, new HarmonyMethod(typeof(NebulaHotfix).GetMethod("GameTick_Transpiler")));
-
-            classType = AccessTools.TypeByName("NebulaWorld.Multiplayer");
-            harmony.Patch(AccessTools.Method(classType, "HostGame"), new HarmonyMethod(typeof(NebulaHotfix).GetMethod("BeforeHostGame")));
+            Type classType;
+            //classType = AccessTools.TypeByName("NebulaWorld.Multiplayer");
+            //harmony.Patch(AccessTools.Method(classType, "HostGame"), new HarmonyMethod(typeof(NebulaHotfix).GetMethod("BeforeHostGame")));
 
             classType = AccessTools.TypeByName("NebulaWorld.SimulatedWorld");
-            harmony.Patch(AccessTools.Method(classType, "SetupInitialPlayerState"), null, new HarmonyMethod(typeof(NebulaHotfix).GetMethod("SetupInitialPlayerState")));
+            harmony.Patch(AccessTools.Method(classType, "SetupInitialPlayerState"),
+                null, new HarmonyMethod(typeof(NebulaHotfix).GetMethod("SetupInitialPlayerState")));
+
+            harmony.Patch(typeof(PlanetTransport).GetMethod("RefreshDispenserOnStoragePrebuildBuild"), 
+                null, null, new HarmonyMethod(typeof(NebulaHotfix).GetMethod("RefreshDispenserOnStoragePrebuildBuild_Transpiler")));
         }
 
         public static void SetupInitialPlayerState()
@@ -72,6 +71,36 @@ namespace NebulaCompatibilityAssist.Patches
             }
         }
 
+        public static IEnumerable<CodeInstruction> RefreshDispenserOnStoragePrebuildBuild_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            try
+            {
+                // factoryModel.gpuiManager is null for remote planets, so we need to use GameMain.gpuiManager which is initialized by nebula
+                // replace : this.factory.planet.factoryModel.gpuiManager
+                // with    : GameMain.gpuiManager
+                var codeMatcher = new CodeMatcher(instructions)
+                    .MatchForward(false,
+                        new CodeMatch(OpCodes.Ldarg_0),
+                        new CodeMatch(OpCodes.Ldfld),
+                        new CodeMatch(OpCodes.Callvirt),
+                        new CodeMatch(OpCodes.Ldfld),
+                        new CodeMatch(i => i.opcode ==OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "gpuiManager")
+                    )
+                    .Repeat(matcher => matcher
+                            .RemoveInstructions(4)
+                            .SetAndAdvance(OpCodes.Call, typeof(GameMain).GetProperty("gpuiManager").GetGetMethod()
+                    ));
+
+                return codeMatcher.InstructionEnumeration();
+            }
+            catch (Exception e)
+            {
+                Log.Warn("RefreshDispenserOnStoragePrebuildBuild_Transpiler fail!");
+                Log.Dev(e);
+                return instructions;
+            }
+        }
+
         public static void BeforeHostGame()
         {
             if (!isPatched)
@@ -84,108 +113,18 @@ namespace NebulaCompatibilityAssist.Patches
                     {
                         //Log.Info(a.GetName()); //why does iterate all assemblies stop the exception?
                     }
-                    Type classType = AccessTools.TypeByName("NebulaNetwork.PacketProcessors.Logistics.DispenserCourierProcessor");
-                    MethodInfo methodInfo = AccessTools.Method(classType, "ProcessPacket", new Type[] { typeof(DispenserCourierPacket), typeof(NebulaConnection) });
-                    Plugin.Instance.Harmony.Patch(methodInfo, new HarmonyMethod(typeof(NebulaHotfix).GetMethod("DispenserCourierProcessor")));
 
-                    classType = AccessTools.TypeByName("NebulaNetwork.PacketProcessors.Planet.PlanetDetailRequestProcessor");
-                    methodInfo = AccessTools.Method(classType, "ProcessPacket", new Type[] { typeof(PlanetDetailRequest), typeof(NebulaConnection) });
-                    Plugin.Instance.Harmony.Patch(methodInfo, new HarmonyMethod(typeof(NebulaHotfix).GetMethod("PlanetDetailRequestProcessor")));
-
-                    Log.Info("DispenserCourierProcessor patch success! (hotfix 0.8.11)");
+                    // Patch PacketProcessors here in the future
+                    Log.Info("PacketProcessors patch success!");
                 }
                 catch (Exception e)
                 {
-                    Log.Warn("DispenserCourierProcessor patch fail!");
+                    Log.Warn("PacketProcessors patch fail!");
                     Log.Warn(e);
                 }
             }
         }
 
-        public static bool PlanetDetailRequestProcessor(PlanetDetailRequest packet, NebulaConnection conn)
-        {
-            PlanetData planetData = GameMain.galaxy.PlanetById(packet.PlanetID);
-            if (!planetData.calculated)
-            {
-                planetData.calculating = true;
-                Task.Run(() =>
-                {
-                    // Modify from PlanetModelingManager.PlanetCalculateThreadMain()
-                    HighStopwatch highStopwatch = new HighStopwatch();
-                    highStopwatch.Begin();
-                    planetData.data = new PlanetRawData(planetData.precision);
-                    planetData.modData = planetData.data.InitModData(planetData.modData);
-                    planetData.data.CalcVerts();
-                    planetData.aux = new PlanetAuxData(planetData);
-                    PlanetAlgorithm planetAlgorithm = PlanetModelingManager.Algorithm(planetData);
-                    planetAlgorithm.GenerateTerrain(planetData.mod_x, planetData.mod_y);
-                    planetAlgorithm.CalcWaterPercent();
-                    if (planetData.type != EPlanetType.Gas)
-                    {
-                        planetAlgorithm.GenerateVegetables();
-                        planetAlgorithm.GenerateVeins();
-                    }
-                    planetData.CalculateVeinGroups();
-                    planetData.GenBirthPoints();
-                    planetData.NotifyCalculated();
-                    // Fix for GS2 that sometimes planetData.runtimeVeinGroups is null 
-                    VeinGroup[] runtimeVeinGroups = planetData.runtimeVeinGroups ?? Array.Empty<VeinGroup>();
-                    conn.SendPacket(new PlanetDetailResponse(planetData.id, runtimeVeinGroups, planetData.landPercent));
-                    Log.Info($"PlanetCalculateThread:{planetData.displayName} time:{highStopwatch.duration:F4}s");
-                });
-                return false;
-            }
-            // Fix for GS2 that sometimes planetData.runtimeVeinGroups is null
-            conn.SendPacket(new PlanetDetailResponse(planetData.id, planetData.runtimeVeinGroups ?? Array.Empty<VeinGroup>(), planetData.landPercent));
-            Log.Info($"Return {planetData.displayName} - {planetData.runtimeVeinGroups}");
-            return false;
-        }
 
-        public static bool DispenserCourierProcessor(DispenserCourierPacket packet)
-        {
-            // Prevent accessing a null dispenser for host
-            PlanetFactory factory = GameMain.mainPlayer.factory;
-            DispenserComponent[] pool = factory?.transport.dispenserPool;
-            if (pool != null && packet.DispenserId > 0 && packet.DispenserId < pool.Length && pool[packet.DispenserId] != null)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public static IEnumerable<CodeInstruction> GameTick_Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            try
-            {
-                // replace : this.CourierDatas[j++] = this.CourierDatas[k];
-                // with    : this.CourierDatas[j] = this.CourierDatas[k];
-                var codeMatcher = new CodeMatcher(instructions)
-                    .MatchForward(false,
-                        new CodeMatch(OpCodes.Ldarg_0),
-                        new CodeMatch(OpCodes.Call),
-                        new CodeMatch(OpCodes.Ldloc_0),
-                        new CodeMatch(OpCodes.Dup),      //+3 => Nop
-                        new CodeMatch(OpCodes.Ldc_I4_1), //+4 => Nop
-                        new CodeMatch(OpCodes.Add),      //+5 => Nop
-                        new CodeMatch(OpCodes.Stloc_0),  //+6 => Nop
-                        new CodeMatch(OpCodes.Ldarg_0),
-                        new CodeMatch(OpCodes.Call),
-                        new CodeMatch(OpCodes.Ldloc_S),
-                        new CodeMatch(OpCodes.Ldelem),
-                        new CodeMatch(OpCodes.Stelem))
-                    .Advance(3)
-                    .SetAndAdvance(OpCodes.Nop, null)
-                    .SetAndAdvance(OpCodes.Nop, null)
-                    .SetAndAdvance(OpCodes.Nop, null)
-                    .SetAndAdvance(OpCodes.Nop, null);
-
-                return codeMatcher.InstructionEnumeration();
-            }
-            catch
-            {
-                Log.Warn("CourierManager.GameTick_Transpiler fail!");
-                return instructions;
-            }
-        }
     }
 }
