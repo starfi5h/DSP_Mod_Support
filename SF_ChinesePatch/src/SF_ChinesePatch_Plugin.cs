654fba9928355225b4eb13c7389f91853b5f37e7
starfish <50672801+starfi5h@users.noreply.github.com>
2023-07-07 02:01:29 +0800

    Add SF_Chinese Patch

---
+++ SF_ChinesePatch/src/SF_ChinesePatch_Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using CommonAPI;
+using CommonAPI.Systems;
+
+namespace SF_ChinesePatch
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInDependency(CommonAPIPlugin.GUID)]
+    [CommonAPISubmoduleDependency(nameof(ProtoRegistry))]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "starfi5h.plugin.SF_ChinesePatch";
+        public const string NAME = "SF_ChinesePatch";
+        public const string VERSION = "1.0.0";
+
+        public static ManualLogSource Log;
+        Harmony harmony;
+
+        public void Awake()
+        {
+            Log = Logger;
+            harmony = new(GUID);
+
+            Log.LogInfo(LDB.strings);
+            NebulaMultiplayer_Patch.OnAwake();
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(StringManager));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIMainMenu), nameof(UIMainMenu._OnOpen))]
+        public static void UIMainMenu_OnOpen_Postfix()
+        {
+            Log.LogWarning("UIMainMenu._OnOpen");
+            Log.LogInfo("New Game (Host)".Translate());
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIOptionWindow), nameof(UIOptionWindow._OnCreate))]
+        public static void UIOptionWindow_OnCreate_Postfix()
+        {
+            Log.LogWarning("UIOptionWindow_OnCreate");
+        }
+
+
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
---
+++ SF_ChinesePatch/src/StringManager.cs
@@ -0,0 +1,97 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace SF_ChinesePatch
+{
+    public class StringManager
+    {
+        private static int lastStringId = 1000;
+        private static List<Proto> protos = new();
+        private static Dictionary<string, int> nameIndices = new ();
+
+        public static void RegisterString(string key, string cnTrans, string enTrans = "")
+        {
+            if (nameIndices.ContainsKey(key))
+                return;
+
+            if (enTrans.Equals("")) enTrans = key;
+            StringProto proto = new()
+            {
+                Name = key,
+                ENUS = enTrans,
+                ZHCN = cnTrans.Equals("") ? enTrans : cnTrans,
+                FRFR = enTrans,
+                ID = FindAvailableStringID()
+            };
+            nameIndices.Add(key, proto.ID);
+            protos.Add(proto);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIGame), nameof(UIGame._OnCreate))]
+        public static void InjectStrings()
+        {
+            // UIGame._OnCreate is triggered earlier than LDBTool injection ()
+            // In order to make translate() works for mod UI, it need to inject earlier
+
+            AddProtosToSet(LDB.strings, protos);
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("me.xiaoye97.plugin.Dyson.LDBTool"))
+            {
+                AccessTools.Field(AccessTools.TypeByName("xiaoye97.LDBTool"), "lastStringId").SetValue(null, lastStringId);
+            }
+        }
+
+        private static void AddProtosToSet<T>(ProtoSet<T> protoSet, List<Proto> protos) where T : Proto
+        {
+            var array = protoSet.dataArray;
+            protoSet.Init(array.Length + protos.Count);
+            for (int i = 0; i < array.Length; i++)
+            {
+                protoSet.dataArray[i] = array[i];
+            }
+
+            for (int i = 0; i < protos.Count; i++)
+            {
+                protoSet.dataArray[array.Length + i] = protos[i] as T;
+                Plugin.Log.LogDebug($"Add {protos[i].ID} {protos[i].Name.Translate()} to {protoSet.GetType().Name}.");
+            }
+
+            var dataIndices = new Dictionary<int, int>();
+            for (int i = 0; i < protoSet.dataArray.Length; i++)
+            {
+                protoSet.dataArray[i].sid = protoSet.dataArray[i].SID;
+                dataIndices[protoSet.dataArray[i].ID] = i;
+            }
+
+            protoSet.dataIndices = dataIndices;
+            if (protoSet is StringProtoSet stringProtoSet)
+            {
+                for (int i = array.Length; i < protoSet.dataArray.Length; i++)
+                {
+                    stringProtoSet.nameIndices[protoSet.dataArray[i].Name] = i;
+                }
+            }
+        }
+
+        private static bool HasStringIdRegisted(int id)
+        {
+            if (LDB.strings.dataIndices.ContainsKey(id)) return true;
+            return false;
+        }
+
+        private static int FindAvailableStringID()
+        {
+            int id = lastStringId + 1;
+            while (true)
+            {
+                if (!HasStringIdRegisted(id))
+                {
+                    break;
+                }
+                id++;
+            }
+            lastStringId = id;
+            return id;
+        }
+    }
+}
