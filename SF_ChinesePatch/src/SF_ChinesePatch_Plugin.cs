15fdffdbecdee411c3dead53d9826587fff6484e
starfish <50672801+starfi5h@users.noreply.github.com>
2023-07-11 03:41:01 +0800

    Add json import

--- SF_ChinesePatch/src/SF_ChinesePatch_Plugin.cs
+++ SF_ChinesePatch/src/SF_ChinesePatch_Plugin.cs
@@ -3,6 +3,7 @@ using BepInEx.Logging;
 using HarmonyLib;
 using CommonAPI;
 using CommonAPI.Systems;
+using System;
 
 namespace SF_ChinesePatch
 {
@@ -25,6 +26,7 @@ namespace SF_ChinesePatch
             Instance = this;
             harmony = new(GUID);
 
+            LoadConfigStrings();
             BulletTime_Patch.OnAwake();
             GalacticScale_Patch.OnAwake(harmony);
             LSTM_Patch.OnAwake();
@@ -36,6 +38,28 @@ namespace SF_ChinesePatch
         public void OnDestroy()
         {
             harmony.UnpatchSelf();
+        }
+
+        public void LoadConfigStrings()
+        {            
+            try
+            {
+                var jsonString = Config.Bind("自定义", "字典", "", @"增加或覆盖翻译。範例格式:\n{""Yes"":""是"",""No"":""否""}").Value;
+                var dict = LightweightJsonParser.ParseJSON(jsonString);
+                if (dict != null)
+                {
+                    Log.LogDebug($"Import {dict.Count} strings from config file");
+                    foreach (var item in dict)
+                    {
+                        StringManager.RegisterString(item.Key, (string)item.Value);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.LogError("Import strings from config file fail!");
+                Log.LogError(e);
+            }
         }
     }
 }
