508f6653e1b5e996d4ce8a11b724bc53a0a44b54
starfish <50672801+starfi5h@users.noreply.github.com>
2023-10-13 07:57:35 +0800

    Fix StationUIManager

---
+++ NebulaCompatibilityAssist/src/Hotfix/Warper0814.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using NebulaModel.Packets.Logistics;
+using NebulaWorld.Logistics;
+
+namespace NebulaCompatibilityAssist.Hotfix
+{
+    public static class Warper0814
+    {
+        #region station
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StationUIManager), nameof(StationUIManager.UpdateStorage))]
+        public static bool UpdateStorage_Prefix(StorageUI packet)
+        {
+            StationComponent stationComponent = StationUIManager.GetStation(packet.PlanetId, packet.StationId, packet.StationGId);
+            if (stationComponent == null || stationComponent.storage?.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        #endregion
+    }
+}
