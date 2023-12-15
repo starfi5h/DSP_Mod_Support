5da0e53d518c4cbda51fe60cde19d20cdf0a8338
starfish <50672801+starfi5h@users.noreply.github.com>
2023-12-15 20:31:52 +0800

    ErrorAnalyzer init

---
+++ ErrorAnalyzer/src/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Bootstrap;
+using HarmonyLib;
+using System.Reflection;
+
+[assembly: AssemblyTitle(ErrorAnalyzer.Plugin.NAME)]
+[assembly: AssemblyVersion(ErrorAnalyzer.Plugin.VERSION)]
+
+namespace ErrorAnalyzer
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "starfi5h.plugin.ErrorAnalyzer";
+        public const string NAME = "ErrorAnalyzer";
+        public const string VERSION = "1.0.0";
+
+        public static ManualLogSource Log;
+        static Harmony harmony;
+
+        public void Awake()
+        {
+            Log = Logger;
+            harmony = new Harmony(GUID);
+            if (!Chainloader.PluginInfos.TryGetValue("dsp.nebula-multiplayer", out var _))
+            {
+                harmony.PatchAll(typeof(UIFatalErrorTip_Patch));
+            }
+            if (!Chainloader.PluginInfos.TryGetValue("NebulaCompatibilityAssist", out var _))
+            {
+                harmony.PatchAll(typeof(StacktraceParser));
+            }
+        }
+
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+            harmony = null;
+        }
+    }
+}
---
+++ ErrorAnalyzer/src/UIFatalErrorTip_Patch.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using BepInEx.Bootstrap;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace ErrorAnalyzer
+{
+    [HarmonyPatch(typeof(UIFatalErrorTip))]
+    internal class UIFatalErrorTip_Patch
+    {
+        private static GameObject button1;
+
+        [HarmonyPostfix]
+        [HarmonyPatch("_OnOpen")]
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Original Function Name")]
+        private static void _OnOpen_Postfix()
+        {
+            try
+            {
+                if (button1 == null)
+                {
+                    var errorPanel = GameObject.Find("UI Root/Overlay Canvas/Fatal Error/errored-panel/");
+                    errorPanel.transform.Find("tip-text-0").GetComponent<Text>().text = Title();
+                    Object.Destroy(errorPanel.transform.Find("tip-text-0").GetComponent<Localizer>());
+                    errorPanel.transform.Find("tip-text-1").GetComponent<Text>().text = Title();
+                    Object.Destroy(errorPanel.transform.Find("tip-text-1").GetComponent<Localizer>());
+
+                    button1 = GameObject.Find("UI Root/Overlay Canvas/In Game/Windows/Dyson Sphere Editor/Dyson Editor Control Panel/hierarchy/layers/blueprint-group/blueprint-2/copy-button");
+                    button1 = Object.Instantiate(button1, errorPanel.transform);
+                    button1.name = "Copy button";
+                    button1.transform.localPosition = errorPanel.transform.Find("icon").localPosition + new Vector3(30, -35, 0);
+                    button1.GetComponent<Image>().color = new Color(0.6f, 0.1f, 0.1f, 0.6f);
+                    
+                    button1.GetComponent<UIButton>().BindOnClickSafe(OnClick1);
+                    ref var tips = ref button1.GetComponent<UIButton>().tips;
+                    tips.tipTitle = "Copy Error";
+                    tips.tipText = "Copy the message to clipboard and close error.";
+                    tips.corner = 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogWarning($"UIFatalErrorTip button did not patch! {e}");
+            }
+        }
+
+        private static string Title()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("An error has occurred! Game version ");
+            stringBuilder.Append(GameConfig.gameVersion.ToString());
+            stringBuilder.Append('.');
+            stringBuilder.Append(GameConfig.gameVersion.Build);
+            stringBuilder.AppendLine();
+            stringBuilder.Append(Chainloader.PluginInfos.Values.Count + " Mods used: ");
+            foreach (var pluginInfo in Chainloader.PluginInfos.Values)
+            {
+                stringBuilder.Append('[');
+                stringBuilder.Append(pluginInfo.Metadata.Name);
+                stringBuilder.Append(pluginInfo.Metadata.Version);
+                stringBuilder.Append("] ");
+            }
+            return stringBuilder.ToString();
+        }
+
+        private static void OnClick1(int id)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("```ini");
+            stringBuilder.AppendLine(Title());
+            var subs = UIFatalErrorTip.instance.errorLogText.text.Split('\n', '\r');
+            foreach (var str in subs)
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    continue;
+                }
+
+                // Remove hash string
+                var start = str.LastIndexOf(" <", StringComparison.Ordinal);
+                var end = str.LastIndexOf(">:", StringComparison.Ordinal);
+                if (start != -1 && end > start)
+                {
+                    stringBuilder.AppendLine(str.Remove(start, end - start + 2));
+                }
+                else
+                {
+                    stringBuilder.AppendLine(str);
+                }
+            }
+            // Apply format for ini code style
+            stringBuilder.Replace(" (at", ";(");
+            stringBuilder.Replace(" inIL_", " ;IL_");
+            stringBuilder.AppendLine("```");
+
+            // Copy string to clipboard
+            GUIUtility.systemCopyBuffer = stringBuilder.ToString();
+            UIFatalErrorTip.ClearError();
+            Object.Destroy(button1);
+            button1 = null;
+        }
+    }
+}
