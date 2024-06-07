ff386dc7685bb693a23be450591ed882885cce83
starfish <50672801+starfi5h@users.noreply.github.com>
2024-06-08 05:55:04 +0800

    [NCA] Sync Relic and SkillPoints in DSP_Battle

---
+++ NebulaCompatibilityAssist/src/Packets/NC_BattleUpdate.cs
@@ -0,0 +1,54 @@
+﻿using NebulaAPI.Networking;
+using NebulaAPI.Packets;
+using NebulaCompatibilityAssist.Patches;
+using NebulaWorld;
+using System.Collections.Generic;
+
+namespace NebulaCompatibilityAssist.Packets
+{
+    public class NC_BattleUpdate
+    {
+        public string Username { get; set; }
+        public EType Type { get; set; }
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public int[] Values1 { get; set; }
+        public int[] Values2 { get; set; }
+
+        public NC_BattleUpdate() { }
+        public NC_BattleUpdate(EType type, int value1, int value2)
+        {
+            Username = Multiplayer.Session.LocalPlayer.Data.Username;
+            Type = type;
+            Value1 = value1;
+            Value2 = value2;
+            Values1 = Values2 = new int[0];
+        }
+        public NC_BattleUpdate(EType type, int[] values1, int[] values2)
+        {
+            Username = Multiplayer.Session.LocalPlayer.Data.Username;
+            Type = type;
+            Values1 = values1;
+            Values2 = values2;
+        }
+
+
+        public enum EType
+        {
+            None,
+            AddRelic,
+            RemoveRelic,
+            ApplyAuthorizationPoint,
+            ResetAuthorizationPoint
+    }
+    }
+
+    [RegisterPacketProcessor]
+    internal class NC_BattleUpdateProcessor : BasePacketProcessor<NC_BattleUpdate>
+    {
+        public override void ProcessPacket(NC_BattleUpdate packet, INebulaConnection conn)
+        {
+            DSP_Battle_Patch.OnReceive(packet);
+        }
+    }
+}
--- NebulaCompatibilityAssist/src/Patches/DSP_Battle_Patch.cs
+++ NebulaCompatibilityAssist/src/Patches/DSP_Battle_Patch.cs
@@ -1,7 +1,9 @@
 ﻿using crecheng.DSPModSave;
 using HarmonyLib;
 using NebulaAPI;
+using NebulaCompatibilityAssist.Hotfix;
 using NebulaCompatibilityAssist.Packets;
+using NebulaWorld;
 using System;
 using System.Reflection;
 
@@ -13,7 +15,7 @@ namespace NebulaCompatibilityAssist.Patches
     {
         private const string NAME = "DSP_Battle";
         private const string GUID = "com.ckcz123.DSP_Battle";
-        private const string VERSION = "3.0.5";
+        private const string VERSION = "3.1.2";
 
         private static IModCanSave Save;
 
@@ -38,6 +40,7 @@ namespace NebulaCompatibilityAssist.Patches
                     Import(bytes);
                 };
 
+                harmony.PatchAll(typeof(Warper));
                 Log.Info($"{NAME} - OK");
             }
             catch (Exception e)
@@ -77,6 +80,100 @@ namespace NebulaCompatibilityAssist.Patches
                 Log.Dev($"AssemblerVerticalConstruction import data");
                 using var p = NebulaModAPI.GetBinaryReader(bytes);
                 Save.Import(p.BinaryReader);
+            }
+        }
+
+        public static void OnReceive(NC_BattleUpdate packet)
+        {
+            Warper.HandleRequest(packet);
+        }
+
+        private static class Warper
+        {
+
+            private static bool IsIncoming { get; set; }
+
+            public static void HandleRequest(NC_BattleUpdate packet)
+            {
+                string message = $"{packet.Username} {packet.Type} ";
+                Log.Info(message + $"{packet.Value1} {packet.Value2}");
+                IsIncoming = true;
+                try
+                {
+                    switch (packet.Type)
+                    {
+                        case NC_BattleUpdate.EType.AddRelic:
+                        {
+                            DSP_Battle.Relic.AddRelic(packet.Value1, packet.Value2);
+                            DSP_Battle.UIRelic.RefreshSlotsWindowUI();
+                            message += ("遗物名称" + packet.Value1.ToString() + "-" + packet.Value2.ToString()).Translate().Split('\n')[0];
+                            break;
+                        }
+                        case NC_BattleUpdate.EType.RemoveRelic:
+                        {
+                            DSP_Battle.Relic.RemoveRelic(packet.Value1, packet.Value2);
+                            DSP_Battle.UIRelic.RefreshSlotsWindowUI();
+                            message += ("遗物名称" + packet.Value1.ToString() + "-" + packet.Value2.ToString()).Translate().Split('\n')[0];
+                            break;
+                        }
+                        case NC_BattleUpdate.EType.ApplyAuthorizationPoint:
+                        {
+                            DSP_Battle.UISkillPointsWindow.ClearTempLevelAdded();
+                            Array.Copy(packet.Values1, DSP_Battle.UISkillPointsWindow.tempLevelAddedL, Math.Min(packet.Values1.Length, DSP_Battle.UISkillPointsWindow.tempLevelAddedL.Length));
+                            Array.Copy(packet.Values2, DSP_Battle.UISkillPointsWindow.tempLevelAddedR, Math.Min(packet.Values2.Length, DSP_Battle.UISkillPointsWindow.tempLevelAddedR.Length));
+                            DSP_Battle.SkillPoints.ConfirmAll();
+                            break;
+                        }
+                        case NC_BattleUpdate.EType.ResetAuthorizationPoint:
+                        {
+                            DSP_Battle.SkillPoints.ResetAll();
+                            break;
+                        }
+                    }
+                    ChatManager.ShowMessageInChat(message);
+                }
+                catch (Exception e)
+                {
+                    ChatManager.ShowWarningInChat("DSP_Battle_Patch error!\n" + e);
+                }
+                IsIncoming = false;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(DSP_Battle.Relic), nameof(DSP_Battle.Relic.AddRelic))]
+            static void AddRelic_Postfix(int type, int num)
+            {
+                if (!Multiplayer.IsActive || IsIncoming) return;
+
+                Log.Info(System.Environment.StackTrace);
+
+                Multiplayer.Session.Network.SendPacket(new NC_BattleUpdate(NC_BattleUpdate.EType.AddRelic, type, num));
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(DSP_Battle.Relic), nameof(DSP_Battle.Relic.RemoveRelic))]
+            static void RemoveRelic_Postfix(int removeType, int removeNum)
+            {
+                if (!Multiplayer.IsActive || IsIncoming) return;
+                Multiplayer.Session.Network.SendPacket(new NC_BattleUpdate(NC_BattleUpdate.EType.RemoveRelic, removeType, removeNum));
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(DSP_Battle.SkillPoints), nameof(DSP_Battle.SkillPoints.ConfirmAll))]
+            static void SkillConfirmAll_Prefix()
+            {
+                if (!Multiplayer.IsActive || IsIncoming) return;
+                Multiplayer.Session.Network.SendPacket(new NC_BattleUpdate(NC_BattleUpdate.EType.ApplyAuthorizationPoint,
+                    DSP_Battle.UISkillPointsWindow.tempLevelAddedL, DSP_Battle.UISkillPointsWindow.tempLevelAddedR));
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(DSP_Battle.SkillPoints), nameof(DSP_Battle.SkillPoints.ResetAll))]
+            static void ResetAll_Prefix()
+            {
+                if (!Multiplayer.IsActive || IsIncoming) return;
+                Multiplayer.Session.Network.SendPacket(new NC_BattleUpdate(NC_BattleUpdate.EType.ResetAuthorizationPoint,
+                    0, 0));
             }
         }
     }
