12d02f7b1620850d22e675c158c81ab65b72258a
starfish <50672801+starfi5h@users.noreply.github.com>
2024-08-14 21:39:59 +0800

    [NCA] Fix nebula 0.9.7 bugs

--- NebulaCompatibilityAssist/src/Hotfix/NebulaHotfix.cs
+++ NebulaCompatibilityAssist/src/Hotfix/NebulaHotfix.cs
@@ -1,17 +1,22 @@
 ﻿using HarmonyLib;
+using NebulaModel;
+using NebulaModel.DataStructures.Chat;
 using NebulaModel.Networking;
 using NebulaModel.Packets.GameStates;
 using NebulaModel.Packets.Logistics;
 using NebulaModel.Utils;
 using NebulaNetwork;
 using NebulaWorld;
+using NebulaWorld.Chat;
 using NebulaWorld.Combat;
 using NebulaWorld.GameStates;
 using NebulaWorld.Logistics;
+using NebulaWorld.MonoBehaviours.Local.Chat;
 using NebulaWorld.Player;
 using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using TMPro;
 using UnityEngine;
 
 namespace NebulaCompatibilityAssist.Hotfix
@@ -30,10 +35,10 @@ namespace NebulaCompatibilityAssist.Hotfix
             {
                 System.Version nebulaVersion = pluginInfo.Metadata.Version;
                 
-                if (nebulaVersion.Major == 0 && nebulaVersion.Minor == 9 && nebulaVersion.Build == 6)
+                if (nebulaVersion.Major == 0 && nebulaVersion.Minor == 9 && nebulaVersion.Build == 7)
                 {
-                    harmony.PatchAll(typeof(Warper096));
-                    Log.Info("Nebula hotfix 0.9.6 - OK");
+                    harmony.PatchAll(typeof(Warper097));
+                    Log.Info("Nebula hotfix 0.9.7 - OK");
                 }
 
                 ChatManager.Init(harmony);
@@ -74,6 +79,7 @@ namespace NebulaCompatibilityAssist.Hotfix
         [HarmonyPatch(typeof(EnemyDFHiveSystem), nameof(EnemyDFHiveSystem.GameTickLogic))]
         [HarmonyPatch(typeof(EnemyDFHiveSystem), nameof(EnemyDFHiveSystem.KeyTickLogic))]
         [HarmonyPatch(typeof(SkillSystem), nameof(SkillSystem.GameTick))]
+        [HarmonyPatch(typeof(DefenseSystem), nameof(DefenseSystem.GameTick))]
         public static Exception EnemyGameTick_Finalizer(Exception __exception)
         {
             if (__exception != null && !suppressed)
@@ -87,38 +93,126 @@ namespace NebulaCompatibilityAssist.Hotfix
         }
     }
 
-    public static class Warper096
+    public static class Warper097
     {
-        // Fix error when client load planet. 
-        // IndexOutOfRangeException: Index was outside the bounds of the array.
-        // EnemyUnitComponent.RunBehavior_Defense_Ground(System.Int32 formTick, SkillSystem skillSystem, EnemyData[] enemyPool, DFGBaseComponent[] bases, System.Single altitude, EnemyData& enemy);(IL_028B)
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(EnemyManager), nameof(EnemyManager.OnFactoryLoadFinished))]
-        public static void OnFactoryLoadFinished_Postfix(PlanetFactory factory)
+        [HarmonyFinalizer]
+        [HarmonyPatch(typeof(EnemyDFGroundSystem), nameof(EnemyDFGroundSystem.CalcFormsSupply))]
+        public static Exception CalcFormsSupply_Finalizer(Exception __exception)
         {
-            var unitCursor = factory.enemySystem.units.cursor;
-            var unitBuffer = factory.enemySystem.units.buffer;
-            for (var i = 1; i < unitCursor; i++)
+            if (__exception != null)
             {
-                // clear the blocking skill to prevent error due to skills are not all present in client
-                unitBuffer[i].ClearBlockSkill();
+                var msg = "Exception during loading: \n" + __exception.ToString();
+                ChatManager.ShowWarningInChat(msg);
+                Log.Error(msg);
             }
+            return null;
         }
 
-        // Fix error when bomb from other player from accessing the null planetfactory
-        // NullReferenceException: Object reference not set to an instance of an object
-        // Bomb_Explosive.TickSkillLogic (SkillSystem skillSystem, System.Int64 time);(IL_03BE)
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(Bomb_Explosive), nameof(Bomb_Explosive.TickSkillLogic))]
-        [HarmonyPatch(typeof(Bomb_Liquid), nameof(Bomb_Liquid.TickSkillLogic))]
-        [HarmonyPatch(typeof(Bomb_EMCapsule), nameof(Bomb_EMCapsule.TickSkillLogic))]
-        public static void Bomb_TickSkillLogic(ref int ___nearPlanetAstroId, ref int ___life)
+        [HarmonyPatch(typeof(UIStatisticsWindow), nameof(UIStatisticsWindow.ComputePowerTab))]
+        public static bool ComputePowerTab_Prefix(UIStatisticsWindow __instance, PowerStat[] powerPool, long energyConsumption, long factoryIndex)
         {
-            if (___nearPlanetAstroId > 0 && GameMain.spaceSector.skillSystem.astroFactories[___nearPlanetAstroId] == null)
+            if (!Multiplayer.IsActive || Multiplayer.Session.LocalPlayer.IsHost) return true;
+
+            /* This is fix for the power statistics.
+               Originally, this function is iterating through all factories and manually summing up "energyStored" values from their PowerSystems.
+               Since client does not have all factories loaded it would cause exceptions.
+             * This fix is basically replacing this:
+
+                PowerSystem powerSystem = this.gameData.factories[i].powerSystem;
+                int netCursor = powerSystem.netCursor;
+                PowerNetwork[] netPool = powerSystem.netPool;
+                for (int j = 1; j < netCursor; j++)
+                {
+                    num2 += netPool[j].energyStored;
+                }
+
+                With: Multiplayer.Session.Statistics.UpdateTotalChargedEnergy(factoryIndex);
+
+             * In the UpdateTotalChargedEnergy(), the total energyStored value is being calculated no clients based on the data received from the server. */
+
+            long num = __instance.ComputePower(powerPool[0]);
+            __instance.productEntryList.Add(1, num, 0L, energyConsumption);
+            num = __instance.ComputePower(powerPool[1]);
+            __instance.productEntryList.Add(1, 0L, num);
+            num = __instance.ComputePower(powerPool[3]);
+
+            long num2 = UpdateTotalChargedEnergy((int)factoryIndex);
+
+            __instance.productEntryList.Add(2, num, 0L, num2);
+            num = __instance.ComputePower(powerPool[2]);
+            __instance.productEntryList.Add(2, 0L, num);
+            return false;
+        }
+
+        public static long UpdateTotalChargedEnergy(int factoryIndex)
+        {
+            var powerEnergyStoredData = Multiplayer.Session.Statistics.PowerEnergyStoredData;
+            if (powerEnergyStoredData == null || factoryIndex >= powerEnergyStoredData.Length) return 0;
+            return powerEnergyStoredData[factoryIndex];
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ChatUtils), nameof(ChatUtils.IsCommandMessage))]
+        public static bool IsCommandMessage(this ChatMessageType type, ref bool __result)
+        {
+            __result = !(type is ChatMessageType.PlayerMessage or ChatMessageType.PlayerMessagePrivate);
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ChatWindow), nameof(ChatWindow.SendLocalChatMessage))]
+        public static bool SendLocalChatMessage_Prefix(ChatWindow __instance, string text, ChatMessageType messageType, ref ChatMessage __result)
+        {
+            __result = SendLocalChatMessage(__instance, text, messageType);
+            return false;
+        }
+
+        public static ChatMessage SendLocalChatMessage(ChatWindow @this, string text, ChatMessageType messageType)
+        {
+            if (!messageType.IsCommandMessage())
             {
-                // The nearest planetFactory hasn't loaded yet, skip and remove
-                ___life = 0;
+                text = ChatUtils.SanitizeText(text);
             }
+            else
+            {
+                switch (messageType)
+                {
+                    case ChatMessageType.SystemInfoMessage when !Config.Options.EnableInfoMessage:
+                    case ChatMessageType.SystemWarnMessage when !Config.Options.EnableWarnMessage:
+                    case ChatMessageType.BattleMessage when !Config.Options.EnableBattleMessage:
+                        return null;
+                }
+            }
+
+            text = RichChatLinkRegistry.ExpandRichTextTags(text);
+
+            if (@this.messages.Count > ChatWindow.MAX_MESSAGES)
+            {
+                @this.messages[0].DestroyMessage();
+                @this.messages.Remove(@this.messages[0]);
+            }
+
+            var textObj = UnityEngine.Object.Instantiate(@this.textObject, @this.chatPanel);
+            var newMsg = new ChatMessage(textObj, text, messageType);
+
+            var notificationMsg = UnityEngine.Object.Instantiate(textObj, @this.notifier);
+            newMsg.notificationText = notificationMsg.GetComponent<TMP_Text>();
+            var message = notificationMsg.AddComponent<NotificationMessage>();
+            message.Init(Config.Options.NotificationDuration);
+
+            @this.messages.Add(newMsg);
+
+            if (@this.chatWindow.activeSelf)
+            {
+                return newMsg;
+            }
+            if (Config.Options.AutoOpenChat && !messageType.IsCommandMessage())
+            {
+                @this.Toggle(false, false);
+            }
+
+            return newMsg;
         }
     }
 }
